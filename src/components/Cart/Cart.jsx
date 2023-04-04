a3662b76ff30d852235146874c3cd9a61348dac0
erobinofficial <erobin8778@gmail.com>
2023-04-04 22:39:44 +0600

    calculating cart section

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const Cart = ({cart, totalPrice}) => {
-    return (
-        <div>
-            <p>Selected items: {cart.length}</p>
-          <p>Total price: ${totalPrice}</p>
-        </div>
-    );
+const Cart = ({ cart, totalPrice, shipping, totalTax, subTotal, quantity }) => {
+  
+  return (
+    <div className="sticky top-0">
+      <div className="flex items-center justify-center text-2xl">
+        <h1 className="font-bold">Order Summary</h1>
+      </div>
+      <div className="mt-8">
+        <p>Selected items: {quantity}</p>
+        <p>Total price: ${totalPrice}</p>
+        <p>Shipping: ${shipping}</p>
+        <p>Tax: ${totalTax}</p>
+        <h3 className="font-bold">Sub Total: ${subTotal}</h3>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
